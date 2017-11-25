b61b008ec493df8035ff426b37f1beb55c28e829
Pedro Marthon <pedro_marthon@live.com>
2017-11-25 16:47:02 +0200

    reworked css and js files for navbar

---
+++ js/javas.js
@@ -0,0 +1,9 @@
+// toggle class scroll 
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50) {
+        $('.navbar-trans').addClass('afterscroll');
+    } else {
+        $('.navbar-trans').removeClass('afterscroll');
+    }
+
+});
