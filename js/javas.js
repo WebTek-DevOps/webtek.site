e7356f3175a012d593a52c093432ab948ebacbd4
Pedro Marthon <pedro_marthon@live.com>
2017-11-27 15:26:46 +0200

    working on sticky navbar

--- js/javas.js
+++ js/javas.js
@@ -1,4 +1,13 @@
-$(window).scroll(function(event) {
-    var scroll = $(window).scrollTop();
-    document.getElementById("scroller").innerHTML = scroll;
-});
+var navbar = document.getElementById("navbar");
+var header = document.getElementById("header");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+        header.classList.add("navbar-is-stuck")
+    } else {
+        navbar.classList.remove("sticky");
+        header.classList.remove("navbar-is-stuck")
+    }
+}
