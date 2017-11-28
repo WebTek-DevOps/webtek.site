c60a5655a9a7aa54e0cd935bb73fde8f10c6de1a
Pedro Marthon <pedro_marthon@live.com>
2017-11-28 14:20:39 +0200

    fixed navbar shadow on sticky

--- js/javas.js
+++ js/javas.js
@@ -5,9 +5,11 @@ var sticky = navbar.offsetTop;
 function myFunction() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky")
+        navbar.classList.add("myHeader")
         header.classList.add("navbar-is-stuck")
     } else {
         navbar.classList.remove("sticky");
+        navbar.classList.remove("myHeader")
         header.classList.remove("navbar-is-stuck")
     }
 }
